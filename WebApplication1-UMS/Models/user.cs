f170a3d25ddbaaac79655f88031cd2917d40db02
RommerChu <rommer.chu@gmail.com>
2019-12-02 19:49:03 -0500

    Add project files.

---
+++ WebApplication1-UMS/Models/user.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1_UMS.Models
+{
+    public class user
+    {
+        public int id { get; set; }
+        public string name { get; set;}
+        public string email { get; set;}
+        public string address { get; set;}
+        public string phone { get; set;}
+        public bool status { get; set; }
+        
+    }
+}
---
+++ WebApplication1-UMS/Models/user_context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1_UMS.Models
+{
+    public class user_context : DbContext
+    {
+        public user_context(DbContextOptions<user_context> options) : base(options) { }
+
+
+        public DbSet<user> user { get; set; }
+
+    }
+
+
+
+}
